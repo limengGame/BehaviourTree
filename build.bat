c8453ddb1649c837527d508812209764f9e62746
LimengGame <1158096230@qq.com>
2018-06-14 12:43:51 +0800

    bat

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ echo 正在生成APK文件...
 
 REM C:\Program Files\Unity\Editor\Unity.exe -projectPath E:\UnityProject\Tsiu_AIToolkit_Project\Tsiu_AIToolkit_Project -quit -batchmode -executeMethod JenkinsAdapter.Build -logFile build.log
 
-%1 -projectPath %2 -quit -batchmode -executeMethod APKBuild.Build -logFile build.log
+%1 -projectPath %2 -quit -batchmode -executeMethod JenkinsAdapter.Build -logFile build.log
 
 echo APK文件生成完毕!  
 pause
