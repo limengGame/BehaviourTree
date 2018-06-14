e513a520901c2149a042742c33580193e62d6dbb
LimengGame <1158096230@qq.com>
2018-06-14 10:57:47 +0800

    bat

--- Tsiu_AIToolkit_Project/build.bat
+++ Tsiu_AIToolkit_Project/build.bat
@@ -1,1 +1,3 @@
-C:\Program Files\Unity\Editor\Unity.exe -projectPath E:\UnityProject\Tsiu_AIToolkit_Project\Tsiu_AIToolkit_Project -executeMethod JenkinsAdapter.Build -quit -batchmode -logFile build.log
+C:
+cd C:\Program Files\Unity\Editor
+Unity.exe -projectPath E:\UnityProject\Tsiu_AIToolkit_Project\Tsiu_AIToolkit_Project -executeMethod JenkinsAdapter.Build -quit -batchmode -logFile build.log
