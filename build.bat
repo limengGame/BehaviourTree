33c5739d26ebcb43438d87368772429dca54034d
unknown <S4354@wowshga.internal>
2020-05-29 18:01:08 +0800

    build工具修改

--- build.bat
+++ build.bat
@@ -2,9 +2,11 @@
 
 echo 正在生成APK文件...  
 
-REM C:\Program Files\Unity\Editor\Unity.exe -projectPath E:\UnityProject\Tsiu_AIToolkit_Project\Tsiu_AIToolkit_Project -quit -batchmode -executeMethod JenkinsAdapter.Build -logFile build.log
+REM C:\Program Files\Unity5.6.7\Editor\Unity.exe -projectPath C:\Resource\Project\Download\BehaviourTree\Tsiu_AIToolkit_Project -quit -batchmode -executeMethod JenkinsAdapter.Build -logFile build.log
 
-%1 -projectPath=%2 -quit -batchmode -executeMethod JenkinsAdapter.Build -logFile build.log
+C:
+cd C:\Program Files\Unity5.6.7\Editor\
+Unity.exe -projectPath C:\Resource\Project\Download\BehaviourTree\Tsiu_AIToolkit_Project -quit -batchmode -executeMethod JenkinsAdapter.Build -logFile build.log
 
 echo APK文件生成完毕!  
 pause
