fd09d6da7fe5b66c19c834cd19a319e5a7d72d2d
unknown <S4354@wowshga.internal>
2020-05-30 14:16:27 +0800

    JenkinsAdapter

--- build.bat
+++ build.bat
@@ -5,8 +5,7 @@ echo 正在生成APK文件...
 REM C:\Program Files\Unity5.6.7\Editor\Unity.exe -projectPath C:\Resource\Project\Download\BehaviourTree\Tsiu_AIToolkit_Project -quit -batchmode -executeMethod JenkinsAdapter.Build -logFile build.log
 
 C:
-cd C:\Program Files\Unity5.6.7\Editor\
-Unity.exe -projectPath C:\Resource\Project\Download\BehaviourTree\Tsiu_AIToolkit_Project -quit -batchmode -executeMethod JenkinsAdapter.Build -logFile build.log
+cd C:\Program Files\Unity5.6.7\Editor\Unity.exe -projectPath C:\Resource\Project\Download\BehaviourTree\Tsiu_AIToolkit_Project -quit -batchmode -executeMethod JenkinsAdapter.Build -logFile build.log
 
 echo APK文件生成完毕!  
 pause
